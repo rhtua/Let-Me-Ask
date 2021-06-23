8122db96359f29dd86ad5fb05215d91fa6bb18ec
rhtua <artoliveira13@gmail.com>
2021-06-23 02:08:41 -0300

    Autenticação

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import { Home } from "./pages/Home";
-import { NewRoom } from "./pages/NewRoom";
+import {BrowserRouter, Route} from "react-router-dom";
+import {Home} from "./pages/Home";
+import {NewRoom} from "./pages/NewRoom";
+import {AuthContextProvider} from "./contexts/AuthContext";
+
 
 function App() {
-  return <NewRoom />;
+    return (
+        <BrowserRouter>
+            <AuthContextProvider>
+                <Route path="/" exact component={Home}/>
+                <Route path="/rooms/new" component={NewRoom}/>
+            </AuthContextProvider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,49 @@
+import {Button} from "../components/Button";
+import {useHistory} from 'react-router-dom';
+
 import illustrationImage from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImage from "../assets/images/google-icon.svg";
-import { Button } from "../components/Button";
 import "../styles/auth.scss";
+import {useContext} from "react";
+import {AuthContext} from "../contexts/AuthContext";
+import {useAuth} from "../hooks/UseAuth";
 
 export function Home() {
-  return (
-    <div id="page-auth">
-      <aside>
-        <img
-          src={illustrationImage}
-          alt="Ilustração descrevendo perguntas e respostas"
-        />
-        <strong>Crie salas de Q&amp;A ao vivo</strong>
-        <p>Tire dúvidas da sua audiência em tempo real</p>
-      </aside>
-      <main>
-        <div className="main-content">
-          <img src={logoImg} alt="Let Me Ask" />
-          <button className="create-room">
-            <img src={googleIconImage} alt="Logo do Google" />
-            Crie sua sala com o Google
-          </button>
-          <div className="separator">ou entre em uma sala</div>
-          <form>
-            <input type="text" placeholder="Digite o código da sala" />
-            <Button type="submit">Entrar na sala</Button>
-          </form>
+    const history = useHistory();
+    const { user, signInWithGoogle } = useAuth();
+
+    async function handleCreateRoom() {
+        if(!user){
+           await signInWithGoogle();
+        }
+        history.push('/rooms/new');
+    }
+    
+    return (
+        <div id="page-auth">
+            <aside>
+                <img
+                    src={illustrationImage}
+                    alt="Ilustração descrevendo perguntas e respostas"
+                />
+                <strong>Crie salas de Q&amp;A ao vivo</strong>
+                <p>Tire dúvidas da sua audiência em tempo real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Let Me Ask"/>
+                    <button className="create-room" onClick={handleCreateRoom}>
+                        <img src={googleIconImage} alt="Logo do Google"/>
+                        Crie sua sala com o Google
+                    </button>
+                    <div className="separator">ou entre em uma sala</div>
+                    <form>
+                        <input type="text" placeholder="Digite o código da sala"/>
+                        <Button type="submit">Entrar na sala</Button>
+                    </form>
+                </div>
+            </main>
         </div>
-      </main>
-    </div>
-  );
+    );
 }
